202b502333a2eba08716d3ec9e9a58e0f098c520
Andy <Andy@Admins-MacBook-Air.local>
2017-03-24 22:20:43 -0400

    buttons

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,1 +1,29 @@
-var animals = [];
+var animals = ["tiger", "bear", "dog", "lion", "rhino", "ostrich", "monkey"];
+
+
+function renderButtons(){
+
+	$("#animal-buttons").empty();
+
+	for(i=0; i<animals.length; i++){
+
+		newBtn = $("<button class='btn btn-lg btn-primary animal'>")
+		newBtn.attr("data-name", animals[i]);
+		newBtn.html(animals[i]);
+		$("#animal-buttons").append(newBtn);
+	};
+
+};
+
+renderButtons();
+
+$(".submit").on("click", function(){
+
+	event.preventDefault();
+
+	var newAnimal = $("#animal-input").val().trim();
+
+	animals.push(newAnimal);
+
+	renderButtons();
+});
